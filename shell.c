c22f1961ce58d7b04b3381bfe3f2eebc0e88de9e
Diego Monfort <diegomonfort12346@gmail.com>
2021-12-02 10:15:18 -0800

    add more fun

--- shell.c
+++ shell.c
@@ -26,9 +26,10 @@ void toktok(char *line, char **argv)
 
 void execute(char **argv)
 {
-	pid_t pid = fork();
+	pid_t pid;
 	int status;
 
+	pid = fork();
 	if (pid < 0)
 	{
 		exit(1);
@@ -45,4 +46,32 @@ void execute(char **argv)
 		while (wait(&status) != pid)
 			;
 	}
+}
+/**
+*main -main shell
+*Return: 0
+*/
+int main(void)
+{
+	char line[1024];
+	char *argv[64];
+	char *p = "# ";
+	char *buffer;
+	size_t bufsize = 32;
+	size_t car;
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+		return ('\0');
+	while (1)
+	{
+		write(1, p, 2);
+		car = getline(&buffer, &bufsize, stdin);
+		if (strcmp(line, "exit\n") == 0)
+			break;
+		toktok(line, argv);
+		execute(argv);
+	}
+	free(buffer);
+	return (0);
 }
