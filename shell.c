c421c26c87e0b7c13695d4e25f103d65a1dfb432
Diego Monfort <diegomonfort12346@gmail.com>
2021-12-02 04:56:31 -0800

    betty changes

--- shell.c
+++ shell.c
@@ -3,23 +3,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
-*
-*
-*
+*execute -Recive command line arg, forks a child process and exe
+*@argv: Argument
 */
 
 void execute(char **argv)
 {
-	pid_t pid;
+	pid_t pid = fork();
 	int status;
 
-	if((pid = fork()) < 0)
+	if (pid < 0)
 	{
 		exit(1);
 	}
 	else if (pid == 0)
 	{
-		if(execvp(*argv, argv) < 0)
+		if (execvp(*argv, argv) < 0)
 		{
 			exit(1);
 		}
