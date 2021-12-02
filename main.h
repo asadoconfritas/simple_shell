f46909eb8c13eb8c8c3832218e79defdee7b47d7
Diego Monfort <diegomonfort12346@gmail.com>
2021-12-02 04:07:06 -0800

    added header

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <>
+#include <>
 
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
