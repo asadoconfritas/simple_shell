f961d404dfff3219ee30f3909e3dd9ca1ebefa6b
Diego Monfort <diegomonfort12346@gmail.com>
2021-12-07 07:03:17 -0800

    Main

--- main.h
+++ main.h
@@ -4,10 +4,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <>
-#include <>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
 
-char *_strcat(char *dest, char *src);
+char *_getline();
+char **tokenoide(char *line);
+char _strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 
