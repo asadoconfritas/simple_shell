423caadb66ebc1b57e57b85745099531a5cc0d24
Diego Monfort <diegomonfort12346@gmail.com>
2021-12-07 07:04:03 -0800

    Main Shell

--- shell.c
+++ shell.c
@@ -3,75 +3,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
-*toktok -Parse into token the input lines
-*@line: input line
-*@argv: arguments
-*/
-void toktok(char *line, char **argv)
-{
-	while (*line != '\0')
-	{
-		while (*line == ' ')
-			*line++ = '\0';
-		*argv++ = line;
-		while (*line != '\0' && *line != ' ')
-			line++;
-	}
-	*argv = '\0';
-}
-/**
-*execute -Recive command line arg, forks a child process and exe
+*prompt -Inifinite loop for the prompt
 *@argv: Argument
 */
-
-void execute(char **argv)
+void prompt(char *argv[])
 {
-	pid_t pid;
-	int status;
+	char *line;
+	char **arguments;
+	int i;
 
-	pid = fork();
-	if (pid < 0)
-	{
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		if (execvp(*argv, argv) < 0)
-		{
-			exit(1);
-		}
-	}
-	else
-	{
-		while (wait(&status) != pid)
-			;
-	}
-}
-/**
-*main -main shell
-*Return: 0
-*/
-int main(void)
-{
-	char line[1024];
-	char *argv[64];
-	char *p = "# ";
-	char *buffer;
-	size_t bufsize = 32;
-	size_t car;
-
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-		return ('\0');
 	while (1)
 	{
-		write(1, p, 2);
-		car = getline(&buffer, &bufsize, stdin);
-		if (strcmp(line, "exit\n") == 0)
-			break;
-		toktok(line, argv);
-		execute(argv);
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "# ", 2);
+		i++;
+
+		line = _getline();
+		arguments = tokenoide(line);
+		execute_command(arguments, argv, i, line);
+
+		free(line);
+		free(arguments);
 	}
-	free(buffer);
-	return (0);
 }
