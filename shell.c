9bc086b551ba814a4df4646b878f91ca10ed9991
Diego Monfort <diegomonfort12346@gmail.com>
2021-12-08 07:39:04 -0800

    Main Program

--- shell.c
+++ shell.c
@@ -1,28 +1,73 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <sys/types.h>
+
 /**
-*prompt -Inifinite loop for the prompt
-*@argv: Argument
+*parse -Take input lines and parse into token
+*@line: Input line
+*@argv: Arguments
 */
-void prompt(char *argv[])
+void parse(char *line, char **argv)
 {
-	char *line;
-	char **arguments;
-	int i;
-
-	while (1)
+	while (*line != '\0')/** if not the end of line... */
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "# ", 2);
-		i++;
+		while (*line == ' ' || *line == '\t' || *line == '\n')
+		{
+			*line++ = '\0';/** replace white spaces with 0*/
+		}
+	*argv++ = line;/** save the argument position*/
+	while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
+		line++;/**skip the argument until...*/
+	}
+	*argv = '\0';/**mark the end of argument list*/
+}
+/**
+*execute -Receives command line arg list (first file name)
+*@argv: arguments
+*/
 
-		line = _getline();
-		arguments = tokenoide(line);
-		execute_command(arguments, argv, i, line);
+void execute(char **argv)
+{
+	pid_t pid;
+	int status;
 
-		free(line);
-		free(arguments);
+	if ((pid = fork()) < 0)/**fork a child process*/
+	{
+		printf("*** ERROR: forking child process failed\n");
+		exit(1);
+	}
+	else if (pid == 0)/**for the child process:*/
+	{
+		if (execvp(*argv, argv) < 0)/**execute the command*/
+		{
+			write(1, "*** ERROR: exec failed\n", 23);
+			exit(1);
+		}
+	}
+	else/**for the parent:*/
+	{
+		while (wait(&status) != pid)/**wait for completion*/
+			;
+	}
+}
+
+/**
+*main -Main proogram shell
+*/
+
+void main(void)
+{
+	char line[1024];/** the input line*/
+	char *argv[64];/** the command line argument*/
+
+	while (1)/** repeat until done ...*/
+	{
+		write(1,"$ ", 2);/**display a prompt*/
+		gets(line);/**read in the command line*/
+		printf("\n");
+		parse(line, argv);/**parse the line*/
+		if (strcmp(argv[0], "exit") == 0)/** is it an "exit"?*/
+			exit(0);/**exit if it is*/
+		execute(argv);/**otherwise, execute the command*/
 	}
 }
