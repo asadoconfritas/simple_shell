ffdd2731a4f820b6d6a80143680731f9e36e5e06
root <guzchelsea24@gmail.com>
2021-12-10 18:16:44 -0800

    cleaning stuff

--- firstshell/generate-authors.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo \# This file lists all individuals having contributed content to the repository.> AUTHORS
-echo \# For how it is generated, see \`generate-authors.sh\`.>> AUTHORS
-git log --format=fuller | grep "Author:" | sort -u | cut -c 13- >> AUTHORS
